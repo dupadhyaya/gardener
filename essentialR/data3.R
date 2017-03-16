cbfc3365e11c50c46c7fb3e3bb4cda2c31fef03d
dupadhyaya <dup1966@gmail.com>
2017-03-16 22:20:58 +0530

    p

--- essentialR/data3.R
+++ essentialR/data3.R
@@ -20,3 +20,35 @@ newnum = c(1.5, 2.3, 4.7)
 newnum[-2]  # all except 2 item
 
 newarr = array(1:12, dim=c(2,3,2))
+newarr
+newarr[,c(1,3),2]
+newarr[,c(2,3),1]
+
+#$------------
+newlm$coefficients
+names(newlm)
+names(newdf)
+newdf$col1
+newdf
+newdf$col2 = c(10,11,12)
+newdf$col2
+
+#droplevels -----------
+droplevels(x,except,...)
+#drop unused levels
+data("InsectSprays")
+str(InsectSprays)
+levels(InsectSprays$spray)
+table(InsectSprays$spray)
+ISs = subset(InsectSprays, spray != 'C')
+head(ISs)
+str(ISs)
+levels(ISs$spray)
+table(ISs$spray)
+ISd = droplevels(ISs)
+table(ISd$spray)
+
+
+#resample ---------Pg 112
+
+
